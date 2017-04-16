1c64ff8a4679ed3a9336faec83d77fc4d62cdacf
William Sims <simsw@os-class.engr.oregonstate.edu>
2017-04-15 18:45:58 -0700

    Added refactored functions, still need to update switch cases.

--- projects/simsw/dominion/dominion.h
+++ projects/simsw/dominion/dominion.h
@@ -130,10 +130,12 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int playAdventurer(struct gameState *state);
-int playSmithy(struct gameState *state, int handPos);
+int playAdventurer(struct gameState *state, int currentPlayer, int drawntreasure, int cardDrawn, int temphand[], int z);
+int playSmithy (struct gameState *state, int handPos, int currentPlayer);
+int playMinion(struct gameState *state, int handPos, int currentPlayer, int choice1, int choice2);
+int playEmbargo(struct gameState *state, int handPos, int currentPlayer, int choice1);
 int playVillage(struct gameState *state, int handPos);
-int playFeast(struct gameState *state, int choice1);
+int playFeast (struct gameState *state,  int choice1, int currentPlayer, int temphand[]);
 int playCouncil_Room(struct gameState *state, int handPos);
 
 
