96bdd903d0169f6073fca21b2709ab2f339bdb43
William Sims <simsw@flip3.engr.oregonstate.edu>
2017-04-27 19:39:11 -0700

    Created test files and updated Makefile

---
+++ projects/simsw/dominion/cardtest2.c
@@ -0,0 +1,51 @@
+/*
+ * cardtest2.c
+ * Author: Will Sims
+ * Testing the adventurer card
+ */
+
+/*
+ * Include the following lines in your makefile:
+ *
+ * cardtest2: cardtest2.c dominion.o rngs.o
+ *      gcc -o cardtest2 -g  cardtest2.c dominion.o rngs.o $(CFLAGS)
+ */
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "adventurer"
+
+int main() {
+  int newCards = 0;
+  int discarded = 1;
+  int xtraCoins = 0;
+  int shuffledCards = 0;
+  int testResult = 0;
+
+  int i, j, m;
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+  int remove1, remove2;
+  int seed = 1000;
+  int numPlayers = 2;
+  int thisPlayer = 0;
+  struct gameState G, testG;
+  int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+    sea_hag, tribute, smithy, council_room};
+
+  // initialize a game state and player cards
+  initializeGame(numPlayers, k, seed, &G);
+
+  if(testResult)
+      printf("\nTEST SUCCESSFULLY COMPLETED\n\n");
+  else
+      printf("TEST FAILED\n\n");
+
+	return 0;
+}
---
+++ projects/simsw/dominion/unittest3.c
@@ -0,0 +1,51 @@
+/*
+ * unittest3.c
+ *
+
+ */
+
+/*
+ * Include the following lines in your makefile:
+ *
+ * unittest3: unittest3.c dominion.o rngs.o
+ *      gcc -o cardtest1 -g  unittest3.c dominion.o rngs.o $(CFLAGS)
+ */
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTFUNCTION "steward"
+
+int main() {
+  int newCards = 0;
+  int discarded = 1;
+  int xtraCoins = 0;
+  int shuffledCards = 0;
+  int testResult = 0;
+
+  int i, j, m;
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+  int remove1, remove2;
+  int seed = 1000;
+  int numPlayers = 2;
+  int thisPlayer = 0;
+  struct gameState G, testG;
+  int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+    sea_hag, tribute, smithy, council_room};
+
+  // initialize a game state and player cards
+  initializeGame(numPlayers, k, seed, &G);
+
+  if(testResult)
+      printf("\nTEST SUCCESSFULLY COMPLETED\n\n");
+  else
+      printf("TEST FAILED\n\n");
+
+	return 0;
+}
